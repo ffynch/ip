659dafe8e9cd085e71bded998e6b29941610d8cf
ffynch <fynchuaye@gmail.com>
2026-08-18 22:29:42 +0800

    Add greeting and farewell messages

--- src/main/java/Beemo.java
+++ src/main/java/Beemo.java
@@ -1,10 +1,18 @@
 public class Beemo {
     public static void main(String[] args) {
+        String divider = "____________________________________________________________";
         String banner = " ____  _____ _____ __  __  ___  \n"
                 + "| __ )| ____| ____|  \\/  |/ _ \\ \n"
                 + "|  _ \\|  _| |  _| | |\\/| | | | |\n"
                 + "| |_) | |___| |___| |  | | |_| |\n"
                 + "|____/|_____|_____|_|  |_|\\___/ \n";
-        System.out.println(banner);
+
+        System.out.println(divider);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Beemo.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Beemo signing off! See you next time! ૮ ˶ᵔ ᵕ ᵔ˶ ა");
+        System.out.println(divider);
     }
 }
